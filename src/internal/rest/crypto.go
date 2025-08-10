8c2d900c6256b9a43b4ea29b521ef240aaa180d9
Demid <demid.shumakher@gmail.com>
2025-08-11 03:22:03 +0500

    add rest

--- src/internal/rest/crypto.go
+++ src/internal/rest/crypto.go
@@ -3,6 +3,8 @@ package rest
 import (
 	"context"
 	"cryptoserver/domain"
+	"encoding/json"
+	"net/http"
 )
 
 type CryptoService interface {
@@ -13,4 +15,90 @@ type CryptoService interface {
 	GetHistoryBySymbol(ctx context.Context, symbol string) ([]domain.PriceHistory, error)
 	GetStatBySymbol(ctx context.Context, symbol string) (*domain.PriceStats, error)
 	Delete(ctx context.Context, symbol string) error
+}
+
+type CryptoHandler struct {
+	cryptoServ CryptoService
+}
+
+func RegisterCryptoHandler(cs CryptoService, mx *http.ServeMux) {
+	ch := &CryptoHandler{
+		cryptoServ: cs,
+	}
+	mx.HandleFunc("GET /crypto", ch.GetAllHandler)
+	mx.HandleFunc("POST /crypto", ch.AddHandler)
+	mx.HandleFunc("GET /crypto/{symbol}", ch.GetBySymbolHandler)
+	mx.HandleFunc("PUT /crypto/{symbol}/refresh", ch.UpdateHandler)
+	mx.HandleFunc("GET /crypto/{symbol}/history", ch.GetHistoryHandler)
+	mx.HandleFunc("GET /crypto/{symbol}/stats", ch.GetStatHandler)
+	mx.HandleFunc("DELETE /crypto/{symbol}", ch.DeleteHandler)
+}
+
+func (c *CryptoHandler) GetAllHandler(w http.ResponseWriter, r *http.Request) {
+	value, err := c.cryptoServ.GetAll(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(value)
+}
+
+func (c *CryptoHandler) AddHandler(w http.ResponseWriter, r *http.Request) {
+	symbol := r.PathValue("symbol")
+	value, err := c.cryptoServ.Create(r.Context(), symbol)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(value)
+}
+
+func (c *CryptoHandler) GetBySymbolHandler(w http.ResponseWriter, r *http.Request) {
+	symbol := r.PathValue("symbol")
+	value, err := c.cryptoServ.GetBySymbol(r.Context(), symbol)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(value)
+}
+
+func (c *CryptoHandler) UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	symbol := r.PathValue("symbol")
+	value, err := c.cryptoServ.UpdateBySymbol(r.Context(), symbol)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(value)
+}
+
+func (c *CryptoHandler) GetHistoryHandler(w http.ResponseWriter, r *http.Request) {
+	symbol := r.PathValue("symbol")
+	value, err := c.cryptoServ.GetHistoryBySymbol(r.Context(), symbol)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(value)
+}
+
+func (c *CryptoHandler) GetStatHandler(w http.ResponseWriter, r *http.Request) {
+	symbol := r.PathValue("symbol")
+	value, err := c.cryptoServ.GetHistoryBySymbol(r.Context(), symbol)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(value)
+}
+
+func (c *CryptoHandler) DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	symbol := r.PathValue("symbol")
+	value, err := c.cryptoServ.GetHistoryBySymbol(r.Context(), symbol)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(value)
 }
